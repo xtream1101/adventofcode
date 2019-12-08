d49558f5c088d6d0969d648cede9eeb678b569ae
Eddy Hintze <eddy@hintze.co>
2019-12-08 14:12:57 -0500

    Added Readme. Day 1 final

---
+++ 2019/python/Day1/final.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import math
+
+
+def load_input():
+    input_file = os.path.join(os.path.dirname(sys.argv[0]), 'input.txt')
+    with open(input_file) as f:
+        inputs = f.read().splitlines()
+    return inputs
+
+
+def mass_to_fuel(mass):
+    """Get the amount of fuel required for the given mass
+
+    Args:
+        mass (int): Mass that the fuel is needed for
+
+    Returns:
+        int: The amount of fuel needed
+    """
+    return math.floor(int(mass)/3) - 2
+
+
+def part1(module_masses):
+    """
+    What is the sum of the fuel requirements for all of the modules on your spacecraft
+
+    Args:
+        module_masses (list): Masses of all modules
+
+    Returns:
+        int: Total amount of fuel needed
+    """
+    total = 0
+    for mass in module_masses:
+        total += mass_to_fuel(mass)
+    return total
+
+
+def part2(module_masses):
+    """
+    What is the sum of the fuel requirements for all of the modules on your spacecraft
+    when also taking into account the mass of the added fuel?
+
+    Args:
+        module_masses (list): Masses of all modules
+
+    Returns:
+        int: Total amount of fuel needed
+    """
+    total = 0
+    for mass in module_masses:
+        module_total = 0
+        val = mass_to_fuel(mass)
+        while val > 0:
+            module_total += val
+            val = mass_to_fuel(val)
+        total += module_total
+    return total
+
+
+if __name__ == '__main__':
+    print("Day 1:")
+
+    part1_ans = part1(load_input())
+    print("\tPart 1:", part1_ans)
+    assert part1_ans == 3454026
+
+    part2_ans = part2(load_input())
+    print("\tPart 2:", part2_ans)
+    assert part2_ans == 5178170
---
+++ 2019/python/README.md
@@ -0,0 +1,9 @@
+# 2019 Advent of Code
+
+https://adventofcode.com/2019/
+
+Each day will have the following files:
+- `input.txt`: The inputs given to solve the problem
+- `n-1.py`: `n` is the day. This is part1's answer. This code may not be the most efficient or clean, but it was the state of the code when I first got the answer
+- `n-2.py`: Save as above, but for part 2
+- `final.py`: This is the cleaned up code and will run parts 1 & 2 for that day. When I get around to it I like to have a proper version that has clean and documented code. 
