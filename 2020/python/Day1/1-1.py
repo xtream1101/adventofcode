d8d2e584e647d81252334354c1abd684450dc45e
Eddy Hintze <eddy@hintze.co>
2020-12-01 20:20:28 +0000

    Have the test input be in its own file

--- 2020/python/Day1/1-1.py
+++ 2020/python/Day1/1-1.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-input_file = os.path.join(os.path.dirname(sys.argv[0]), 'input.txt')
-with open(input_file) as f:
-    report = f.read().splitlines()
-report = list(map(int, report))
 
-test_report = [1721, 979, 366, 299, 675, 1456]
+def load_input(file_name):
+    input_file = os.path.join(os.path.dirname(sys.argv[0]), file_name)
+    with open(input_file) as f:
+        report = f.read().splitlines()
+    return list(map(int, report))
 
 
 def run(expected_sum, report):
@@ -17,10 +17,10 @@ def run(expected_sum, report):
                 return entry * sub_entry
 
 
-test_ans = run(2020, test_report)
+test_ans = run(2020, load_input('test_input.txt'))
 assert test_ans == 514579
 
-ans = run(2020, report)
+ans = run(2020, load_input('input.txt'))
 assert ans == 902451
 
 print(ans)
