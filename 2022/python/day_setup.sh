36b6e0a1767c9b85d3fe418b13dab9c100aef256
Eddy Hintze <eddy@hintze.co>
2022-12-02 16:57:58 -0500

    Simple script to setup the current days folder

---
+++ 2022/python/day_setup.sh
@@ -0,0 +1,18 @@
+
+# Need to use `source day_setup.sh`, NOT bash or ./ otherwise `cd` will not work
+
+DAY=$(date -d "$D" '+%-d')
+DAY2=$(date -d "$D" '+%d')
+
+DAY_FOLDER="Day${DAY2}"
+
+mkdir -p $DAY_FOLDER
+
+# Do not overwrite if a file exists
+cp -n script_template.py "${DAY_FOLDER}/${DAY}-1.py"
+
+touch "${DAY_FOLDER}/test_input.txt"
+# Could use cookies and wget to get my actual input but this is good enough for now
+touch "${DAY_FOLDER}/input.txt"
+
+cd "${DAY_FOLDER}"
---
+++ 2022/python/script_template.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+
+def load_input(file_name):
+    input_file = os.path.join(os.path.dirname(sys.argv[0]), file_name)
+    with open(input_file) as f:
+        lines = f.read().splitlines()
+    return list(lines)
+
+
+def run(data):
+    total_val = 0
+
+    return total_val
+
+
+test_ans = run(load_input('test_input.txt'))
+print(test_ans)
+assert test_ans == 0000
+
+ans = run(load_input('input.txt'))
+# assert ans == 0000
+print(ans)
