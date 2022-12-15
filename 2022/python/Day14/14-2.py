5df153b39a2108bd2ee242ee8940428bab3958bf
Eddy Hintze <eddy@hintze.co>
2022-12-15 00:06:32 -0500

    Day14 pt2 is fast now. Had dumb logic

--- 2022/python/Day14/14-2.py
+++ 2022/python/Day14/14-2.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import collections
+import time
 
 
 def load_input(file_name):
@@ -10,28 +10,36 @@ def load_input(file_name):
     return lines
 
 
-def render_cave(rocks, sand, start=(500, 0)):
-    y_vals = [ r[1] for r in rocks + collections.deque([start]) ]
-    x_vals = [ r[0] for r in rocks + sand + collections.deque([start]) ]
+def render_cave(filled, falling_grain=None):
+    y_vals = [ r[1] for r in filled.keys() ]
+    x_vals = [ r[0] for r in filled.keys() ]
     # Used to offset the render of the cave
     mm_y = (min(y_vals), max(y_vals))
     mm_x = (min(x_vals), max(x_vals))
 
-    for y in range(start[1], mm_y[1] + 2):
+    for y in range(mm_y[0], mm_y[1] + 2):
         for x in range(mm_x[0], mm_x[1] + 1):
-            if (x,y) in rocks:
-                print('\033[36m#\033[0m', end='')
-            elif (x,y) in sand:
-                print('\033[93m0\033[0m', end='')
-            elif (x,y) == start:
-                print('\033[31m+\033[0m', end='')
-            else:
-                print('\033[90m.\033[0m', end='')
+            value = filled.get((x,y), '.')
+
+            if (x,y) == falling_grain:
+                value = 'o'
+                print('\033[35m', end='')
+            elif value == '#':
+                print('\033[36m', end='')
+            elif value == 'o':
+                print('\033[93m', end='')
+            elif value == '.':
+                print('\033[90m', end='')
+            elif value == '+':
+                print('\033[31m', end='')
+
+            print(f'{value}\033[0m', end='')
+
         print()
     rock_floor = '#' * (mm_x[1] - mm_x[0] + 1)
     print(f'\033[36m{rock_floor}\033[0m', end='')
     print()
-    print(f"Sand: {len(sand)}")
+    print(f"Sand: {list(filled.values()).count('o')}")
 
 
 def get_all_rocks(rock_range):
@@ -65,66 +73,58 @@ def get_all_rocks(rock_range):
     return rocks
 
 
-def run(cave_scan, render=False):
+def run(cave_scan, render_speed=0, render_every=1):
     start = (500, 0)
-    # try and use deque in place of list for its append/pop performance
-    rocks = collections.deque()
-    sand = collections.deque([start])
+    filled = {
+        start: '+'
+    }
     for rock_scan in cave_scan:
         rock_range = [
             tuple(map(int, r.split(','))) for r in rock_scan.split(' -> ')
         ]
-        rocks.extend(get_all_rocks(rock_range))
+        for rock in get_all_rocks(rock_range):
+            filled[rock] = '#'
 
+    cave_floor_y = max([ r[1] for r in filled.keys() ]) + 2
 
-    cave_floor_y = max([ r[1] for r in rocks ]) + 2
-
-    while True:
-        grain = sand.pop()
-        # Just so we know its still running...
-        if grain == start and len(sand) % 1000 == 0:
-            print(f"Sand: {len(sand)}")
-            render_cave(rocks, sand, start=start)
-
-        if render:
-            render_cave(rocks, sand, start=start)
-            input()
+    falling_grain = start
+    while filled[start] != 'o':
+        if render_speed and len(filled) % render_every == 0:
+            # I know the render_every starts off with including the rocks, but does not matter
+            render_cave(filled, falling_grain)
+            time.sleep(render_speed)
+            # input()
 
         # Settled on floor
-        if grain[1] == cave_floor_y - 1:
-            # Just so we know its still running...
-            print(f"Landed on floor at x: {grain[0]}")
-            sand.append(grain)
-            sand.append(start)
+        if falling_grain[1] == cave_floor_y - 1:
+            filled[falling_grain] = 'o'
+            falling_grain = start
 
         # Sand down
-        elif (grain[0], grain[1] + 1) not in rocks and (grain[0], grain[1] + 1) not in sand:
-            sand.append((grain[0], grain[1] + 1))
+        elif not filled.get((falling_grain[0], falling_grain[1] + 1)):
+            falling_grain = (falling_grain[0], falling_grain[1] + 1)
 
-        # Sand Down left
-        elif (grain[0] - 1, grain[1] + 1) not in rocks and (grain[0] - 1, grain[1] + 1) not in sand:
-            sand.append((grain[0] - 1, grain[1] + 1))
+        # Sand down left
+        elif not filled.get((falling_grain[0] - 1, falling_grain[1] + 1)):
+            falling_grain = (falling_grain[0] - 1, falling_grain[1] + 1)
 
-        # Sand Down right
-        elif (grain[0] + 1, grain[1] + 1) not in rocks and (grain[0] + 1, grain[1] + 1) not in sand:
-            sand.append((grain[0] + 1, grain[1] + 1))
+        # Sand down right
+        elif not filled.get((falling_grain[0] + 1, falling_grain[1] + 1)):
+            falling_grain = (falling_grain[0] + 1, falling_grain[1] + 1)
 
-        # Settled
+        # Settled on either rock or other sand
         else:
-            sand.append(grain)
-            if grain == start:
-                # Sand blocking source
-                break
-            sand.append(start)
+            filled[falling_grain] = 'o'
+            falling_grain = start
 
-    render_cave(rocks, sand, start=start)
-    return len(sand)
+    # render_cave(filled, grain)
+    return list(filled.values()).count('o')
 
 
-test_ans = run(load_input('test_input.txt'), render=False)
+test_ans = run(load_input('test_input.txt'), render_speed=0.005)
 print(test_ans)
 assert test_ans == 93
 
-ans = run(load_input('input.txt'), render=False)
-# assert ans == 862
+ans = run(load_input('input.txt'), render_speed=0, render_every=1000)
+assert ans == 28744
 print(ans)
