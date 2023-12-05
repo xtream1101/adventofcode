f832b952a6543a0b83d775e65fccb6610778e694
Eddy Hintze <eddy@hintze.co>
2023-12-05 16:09:17 -0500

    Day 5 part 2 complete (very slow)

--- 2023/python/Day05/5-1.py
+++ 2023/python/Day05/5-1.py
@@ -19,57 +19,59 @@ def get_next_val(source_item, source_map):
 
 
 def run(data):
-
     almanac = {
-        'seeds': [],
-        'maps': defaultdict(dict),
-        'output': {},
+        "seeds": [],
+        "maps": defaultdict(dict),
+        "output": {},
     }
 
     curr_map = None
     for line in data:
-        if line.strip() == '':
+        if line.strip() == "":
             continue
 
-        if line.startswith('seeds:'):
-            almanac['seeds'] = list(map(int, line.split(':')[-1].split()))
+        if line.startswith("seeds:"):
+            almanac["seeds"] = list(map(int, line.split(":")[-1].split()))
             continue
 
-        if line.endswith('map:'):
-            curr_map = line.split(' map:')[0]
+        if line.endswith("map:"):
+            curr_map = line.split(" map:")[0]
             continue
 
         if curr_map:
             dest_start, source_start, range_len = [int(x) for x in line.split()]
             source = (source_start, source_start + range_len)
             dest = (dest_start, dest_start + range_len)
-            almanac['maps'][curr_map][source] = dest
+            almanac["maps"][curr_map][source] = dest
             continue
 
     smallest_location = None
-    for seed in almanac['seeds']:
-        almanac['output'][seed] = {}
+    for seed in almanac["seeds"]:
+        # This output is only needed for debuging purposes
+        almanac["output"][seed] = {}
 
-        soil = get_next_val(seed, almanac['maps']['seed-to-soil'])
-        almanac['output'][seed]['soil'] = soil
+        soil = get_next_val(seed, almanac["maps"]["seed-to-soil"])
+        almanac["output"][seed]["soil"] = soil
 
-        fertilizer =get_next_val(soil, almanac['maps']['soil-to-fertilizer'])
-        almanac['output'][seed]['fertilizer'] = fertilizer
+        fertilizer = get_next_val(soil, almanac["maps"]["soil-to-fertilizer"])
+        almanac["output"][seed]["fertilizer"] = fertilizer
 
-        water = get_next_val(fertilizer, almanac['maps']['fertilizer-to-water'])
-        almanac['output'][seed]['water'] = water
+        water = get_next_val(fertilizer, almanac["maps"]["fertilizer-to-water"])
+        almanac["output"][seed]["water"] = water
 
-        light = get_next_val(water, almanac['maps']['water-to-light'])
-        almanac['output'][seed]['light'] = light
+        light = get_next_val(water, almanac["maps"]["water-to-light"])
+        almanac["output"][seed]["light"] = light
 
-        temperature = get_next_val(light, almanac['maps']['light-to-temperature'])
-        almanac['output'][seed]['temperature'] = temperature
+        temperature = get_next_val(light, almanac["maps"]["light-to-temperature"])
+        almanac["output"][seed]["temperature"] = temperature
 
-        humidity = get_next_val(temperature, almanac['maps']['temperature-to-humidity'])
-        almanac['output'][seed]['humidity'] = humidity
+        humidity = get_next_val(temperature, almanac["maps"]["temperature-to-humidity"])
+        almanac["output"][seed]["humidity"] = humidity
 
-        location = get_next_val(humidity, almanac['maps']['humidity-to-location'])
-        almanac['output'][seed]['location'] = location
+        location = get_next_val(humidity, almanac["maps"]["humidity-to-location"])
+        almanac["output"][seed]["location"] = location
+
+        print(seed, almanac["output"][seed])
 
         if smallest_location is None:
             smallest_location = location
@@ -81,10 +83,10 @@ def run(data):
     return smallest_location
 
 
-test_ans = run(load_input('test_input.txt'))
+test_ans = run(load_input("test_input.txt"))
 print(test_ans)
 assert test_ans == 35
 
-ans = run(load_input('input.txt'))
+ans = run(load_input("input.txt"))
 assert ans == 26273516
 print(ans)
--- 2023/python/Day05/5-2.py
+++ 2023/python/Day05/5-2.py
@@ -2,6 +2,7 @@ import os
 import sys
 from rich import print
 from collections import defaultdict
+import time
 
 
 def load_input(file_name):
@@ -12,58 +13,65 @@ def load_input(file_name):
 
 
 def get_next_val(source_item, source_map):
-    for source_range, dest_range in source_map.items():
-        if source_item in range(*source_range):
+    for source_range, dest_range in source_map:
+        if source_item >= source_range[0] and source_item <= source_range[1]:
             return source_item - source_range[0] + dest_range[0]
     return source_item
 
 
 def run(data):
-
     almanac = {
-        'seeds': [],
-        'maps': defaultdict(dict),
-        'output': {},
+        "seeds": [],
+        "maps": defaultdict(list),
+        "output": {},
     }
 
     curr_map = None
     for line in data:
-        if line.strip() == '':
+        if line.strip() == "":
             continue
 
-        if line.startswith('seeds:'):
-            raw_seeds = list(map(int, line.split(':')[-1].split()))
+        if line.startswith("seeds:"):
+            raw_seeds = list(map(int, line.split(":")[-1].split()))
             s_iter = iter(raw_seeds)
             for s in s_iter:
-                almanac['seeds'].append((s, next(s_iter)))
+                almanac["seeds"].append((s, next(s_iter)))
             continue
 
-        if line.endswith('map:'):
-            curr_map = line.split(' map:')[0]
+        if line.endswith("map:"):
+            curr_map = line.split(" map:")[0]
             continue
 
         if curr_map:
             dest_start, source_start, range_len = [int(x) for x in line.split()]
             source = (source_start, source_start + range_len)
             dest = (dest_start, dest_start + range_len)
-            almanac['maps'][curr_map][source] = dest
+            almanac["maps"][curr_map].append((source, dest))
             continue
 
+    # convert maped values to tuples
+    # Needed when I was trying to use a memorized fn
+    for k, v in almanac["maps"].items():
+        almanac["maps"][k] = tuple(v)
+
     smallest_location = None
-    for seed_range in almanac['seeds']:
+    ts = time.time()
+    for seed_range in almanac["seeds"]:
         print("Current Range: ", seed_range)
         for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
             if seed % 1000000 == 0:
-                print("Seed: ", seed)
-            soil = get_next_val(seed, almanac['maps']['seed-to-soil'])
-            fertilizer = get_next_val(soil, almanac['maps']['soil-to-fertilizer'])
-            water = get_next_val(fertilizer, almanac['maps']['fertilizer-to-water'])
-            light = get_next_val(water, almanac['maps']['water-to-light'])
-            temperature = get_next_val(light, almanac['maps']['light-to-temperature'])
-            humidity = get_next_val(temperature, almanac['maps']['temperature-to-humidity'])
-            location = get_next_val(humidity, almanac['maps']['humidity-to-location'])
+                print("Seed: ", seed, " Time: ", time.time() - ts)
+                ts = time.time()
 
-            # print(seed, location)
+            soil = get_next_val(seed, almanac["maps"]["seed-to-soil"])
+            fertilizer = get_next_val(soil, almanac["maps"]["soil-to-fertilizer"])
+            water = get_next_val(fertilizer, almanac["maps"]["fertilizer-to-water"])
+            light = get_next_val(water, almanac["maps"]["water-to-light"])
+            temperature = get_next_val(light, almanac["maps"]["light-to-temperature"])
+            humidity = get_next_val(
+                temperature, almanac["maps"]["temperature-to-humidity"]
+            )
+            location = get_next_val(humidity, almanac["maps"]["humidity-to-location"])
 
             if smallest_location is None:
                 smallest_location = location
@@ -73,10 +81,11 @@ def run(data):
     return smallest_location
 
 
-test_ans = run(load_input('test_input.txt'))
+test_ans = run(load_input("test_input.txt"))
 print(test_ans)
 assert test_ans == 46
 
-ans = run(load_input('input.txt'))
-# assert ans == 0000
+ans = run(load_input("input.txt"))
+# Took 2h 2m 40s to run
+assert ans == 34039469
 print(ans)
